416cb570ac13da7a91340212ac37247cca165a69
josejimenez-ad <jose.jimenez@applydigital.com>
2024-12-26 17:54:03 -0300

    feat: adding new pom for the account page

--- page-objects/accountPage.ts
+++ page-objects/accountPage.ts
@@ -1,5 +1,6 @@
 import {Locator} from "@playwright/test";
 import {Page} from "@playwright/test";
+import {LeftNavMenu} from "./commonSections/accountSections/leftNavigationMenu";
 import {Navbar} from "./commonSections/navBar";
 import {SearchBox} from "./commonSections/searchBox";
 import {ContentDivider} from "./commonSections/contentDivider";
@@ -9,11 +10,19 @@ import {CarouselComponent} from "./commonSections/carouselComponent";
 export class AccountPage{
     // Defining the locators
     readonly page: Page;
-
+    readonly leftNavigationMenu: LeftNavMenu;
+    readonly ordersPage: OrdersPage;
+    readonly addressesPage: AddressPage;
+    readonly personalDetailsPage: PersonalDetails;
+    readonly wishlistPage: Wishlist;
+    readonly giftCardsPage: GiftCards;
+    readonly storeCredits: StoreCredits;
+    readonly managePreferences: ManagePreferences;
 
     // constructor
     constructor(page:Page) {
         this.page = page;
+        this.leftNavigationMenu = new LeftNavMenu(this.page.locator('[data-controller="account-nav"]'));
 
 
     }
